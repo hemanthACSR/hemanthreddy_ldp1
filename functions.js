6cd549ed31901f4a4f5040160f9f5a600e84d04e
Ahemanth <hemanth.a@zemosolabs.com>
2025-12-07 19:03:56 +0530

    Added js file for assaignment

---
+++ functions.js
@@ -0,0 +1,19 @@
+//q1
+function greet(name){
+    return "hello " + name;
+}
+
+function call(fn,value){
+    console.log(fn(value));
+}
+
+call(greet,'Hemanth')
+
+//q2
+
+const firstletters=(firstname,lastname) =>{
+    console.log(firstname[0]+lastname[0]);
+
+}
+
+firstletters("Hemanth","Reddy")
